c36bda9c49e4ce6189b374a67710bcebe23bef36
Just a person on the internet <54821768+TheCNerd@users.noreply.github.com>
2022-04-27 09:44:13 -0600

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 fn main() {
     println!("Welcome to Rust Password Generator!");
     println!("At the moment, it isn't finished yet, so this is just a placeholder.");
-    println!("This is a personal project to learn rust then use it in a\nreal-world project, so this program on GitHub doesn't work.");
+    println!("This is a personal project to learn rust then use it in a\nreal-world project, so this program on GitHub doesn't work at the moment.");
 }
